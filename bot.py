a50257fbdf8ab3972fe8ddaf39b6fd7594b81839
membrenolol2-bit <membreno.lol2@gmail.com>
2026-09-01 21:37:50 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ from storage import (
 )
 
 # ── Config ────────────────────────────────────────────────────────────────────
-BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+BOT_TOKEN = os.getenv("MTU0NDQzNjM3Nzg1NTMzMjQ3Mw.GUTH4L.muaUgty9lVfiiVJCnuWKRIOgKESO-J3V0Z9Lkw")
 if not BOT_TOKEN:
     raise ValueError("❌ Missing DISCORD_BOT_TOKEN in Railway Variables")
 
